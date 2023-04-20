9970237d60d9f6f5767bf8dd0bb03b08f1a40c1c
Sanskar123456789 <sanskar.agarwal@retrotax-aci.com>
2023-04-20 11:55:08 +0530

    staticProps & static paths

--- src/pages/products/[prodId].tsx
+++ src/pages/products/[prodId].tsx
@@ -1,13 +1,30 @@
 import { useRouter } from "next/router";
 
-function ProductId() {
+const ProductId :  React.FC<any> = ({product}) => {
     const router = useRouter();
     const Id = router.query.prodId
     return (
         <div>
             Enter {Id}
+            {product.id}
         </div>
     );
 }
 
 export default ProductId;
+export async function getStaticPaths() {
+    return {
+      paths: [
+        { params: { prodId: 'product-1' } },
+        { params: { prodId: 'product-2' } },
+        { params: { prodId: 'product-3' } },
+      ],
+      fallback: false,
+    };
+  }
+export async function getStaticProps() {
+    const response = await fetch('https://dummyjson.com/products/1')
+    const product = await response.json();
+    console.log({props: { product }});
+    return { props: { product } };
+}
