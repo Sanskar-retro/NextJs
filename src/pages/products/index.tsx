38c1303e56941b43dee8014ae20293844234cd8c
Sanskar123456789 <sanskar.agarwal@retrotax-aci.com>
2023-04-21 15:21:30 +0530

    search bar added

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -2,29 +2,58 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Product from '@/components/Product';
 import { ProductModel } from '@/Model/Product';
-
+import { ProgressSpinner } from 'primereact/progressspinner';
+import { InputText } from 'primereact/inputtext';
 
 function index() {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [products, setProducts] = useState([]);
-    const getProducts = async () =>{
-        const data = await axios.get('https://dummyjson.com/products')
-        setProducts(data.data.products);  
-    }
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [productsCopy, setProductsCopy] = useState([]);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [searchProducts, setSearchProducts] = useState('');
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(() => {
        getProducts()
     }, [])
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        let a = productsCopy.filter((product : any) =>{
+            if(product?.title.includes(searchProducts)){
+                return product
+            }
+        })
+        setProducts(a)
+    }, [searchProducts])
+
+    const getProducts = async () =>{
+        const data = await axios.get('https://dummyjson.com/products')
+        setProducts(data.data.products); 
+        setProductsCopy(data.data.products);
+    }
 
     
     return (
-        <div className='flex flex-wrap'>
-            {
-                products && products.map((product : ProductModel) =>{
-                    return <><Product product={product}/></>
-                })
-            }
+        <>
+        <div className='m-4 w-full'>
+            <span className="p-input-icon-left">
+                <i className="pi pi-search" />
+                <InputText placeholder="Search" value={searchProducts} onChange={(e)=>{
+                    setSearchProducts(e.target.value);
+                }}/>
+            </span>
         </div>
+        {products ? 
+            <div className='flex flex-wrap'>
+                {
+                    products.map((product : ProductModel) =>{
+                        return <><Product product={product}/></>
+                    }) 
+                }
+            </div> : 
+            <><ProgressSpinner /></>
+        }
+        </>
     );
 }
 
